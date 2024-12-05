2dff8c15193664f41e65516c3788501b067f6d2e
javohir-abdusattorov <javohirabdusattorov2005@gmail.com>
2024-12-05 21:05:22 +0500

    day #4

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::collections::{BinaryHeap, HashMap};
 use std::io::Read;
 use std::time::Instant;
-use std::iter;
+use std::{iter, vec};
 use itertools::Itertools;
 use regex::Regex;
 
@@ -21,6 +21,8 @@ fn main() {
         (3, 2) => day_3_part_2(input),
         (4, 1) => day_4_part_1(input),
         (4, 2) => day_4_part_2(input),
+        (5, 1) => day_5_part_1(input),
+        (5, 2) => day_5_part_2(input),
         _ => panic!("Solution not implemented for day #{day} part #{part}")
     };
 
@@ -146,18 +148,141 @@ fn day_3_part_1(input: String) -> i32 {
         })
 }
 
-fn day_3_part_2(_: String) -> i32 {
-    0
+fn day_3_part_2(input: String) -> i32 {
+    let mut can = true;
+    Regex
+        ::new(r"(mul\([0-9]+,[0-9]+\))|(don\'t\(\))|(do\(\))")
+        .unwrap()
+        .find_iter(&input)
+        .fold(0, |acc, str| {
+            let str = str.as_str();
+            match str {
+                "do()" => { can = true; acc },
+                "don't()" => { can = false; acc },
+                _ if can => acc + str
+                    .replace(")", "")
+                    .replace("mul(", "")
+                    .split(",")
+                    .filter_map(|str| str.parse::<i32>().ok())
+                    .fold(1, |r, n| r * n),
+                _ => { acc },
+            }
+        })
 }
 
 
-fn day_4_part_1(_: String) -> i32 {
+fn day_4_part_1(input: String) -> i32 {
+    let mut matrix: Vec<Vec<&str>> = vec![];
+    let mut x: Vec<(i32, i32)> = vec![];
+
+    input
+        .split("\n")
+        .for_each(|line| {
+            matrix.push(line
+                .split("")
+                .filter(|str| !str.is_empty())
+                .enumerate()
+                .map(|(i, str)| {
+                    if str == "X" { x.push((matrix.len() as i32, i as i32)); }
+                    str
+                })
+                .collect::<Vec<&str>>()
+            );
+        });
+
+    let limit_y = matrix.len() as i32 - 1;
+    let limit_x = matrix[0].len() as i32 - 1;
+    let word = [ "M", "A", "S" ];
+    let directions = [
+        (-1, -1),
+        (-1, 0),
+        (-1, 1),
+        (0, -1),
+        (0, 1),
+        (1, -1),
+        (1, 0),
+        (1, 1),
+    ];
+
+    x
+        .into_iter()
+        .fold(0, |total, (y, x)| {
+            total + directions
+                .iter()
+                .filter(|direction| {
+                    let mut current_y = y;
+                    let mut current_x = x;
+
+                    word
+                        .iter()
+                        .all(|char| {
+                            current_y += direction.0;
+                            current_x += direction.1;
+
+                            current_y >= 0 && current_x >= 0 && current_y <= limit_y && current_x <= limit_x && &matrix[current_y as usize][current_x as usize] == char
+                        })
+                })
+                .count()
+        }) as i32
+}
+
+fn day_4_part_2(input: String) -> i32 {
+    let mut matrix: Vec<Vec<&str>> = vec![];
+    let mut a: Vec<(i32, i32)> = vec![];
+
+    input
+        .split("\n")
+        .for_each(|line| {
+            matrix.push(line
+                .split("")
+                .filter(|str| !str.is_empty())
+                .enumerate()
+                .map(|(i, str)| {
+                    if str == "A" { a.push((matrix.len() as i32, i as i32)); }
+                    str
+                })
+                .collect::<Vec<&str>>()
+            );
+        });
+
+    let limit_y = matrix.len() as i32 - 1;
+    let limit_x = matrix[0].len() as i32 - 1;
+    let directions = [
+        [(-1, -1), (1, 1)],
+        [(1, -1), (-1, 1)],
+    ];
+
+    a
+        .into_iter()
+        .filter(|(y, x)| directions.iter().all(|direction| {
+            let strs = direction
+                .iter()
+                .filter_map(|direction| {
+                    let y = y + direction.0;
+                    let x = x + direction.1;
+
+                    if y >= 0 && x >= 0 && y <= limit_y && x <= limit_x && (matrix[y as usize][x as usize] == "M" || matrix[y as usize][x as usize] == "S") {
+                        Some(matrix[y as usize][x as usize])
+                    } else {
+                        None
+                    }
+                })
+                .collect::<Vec<&str>>();
+
+            strs.len() == 2 && strs.contains(&"M") && strs.contains(&"S")
+        }))
+        .count() as i32
+}
+
+
+fn day_5_part_1(_: String) -> i32 {
     0
 }
 
-fn day_4_part_2(_: String) -> i32 {
+fn day_5_part_2(_: String) -> i32 {
     0
 }
+
 
 
 fn read_day() -> (i32, i32) {
