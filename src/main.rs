611543e8a36582e0419f7ab1a9fc1fdd65fc0b84
javohir-abdusattorov <javohirabdusattorov2005@gmail.com>
2024-12-05 18:48:50 +0500

    day #1 part #2 and day #2 part #2

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::BinaryHeap;
+use std::collections::{BinaryHeap, HashMap};
 use std::io::Read;
 use std::time::Instant;
 use std::iter;
@@ -6,26 +6,29 @@ use itertools::Itertools;
 use regex::Regex;
 
 fn main() {
-    let day = read_day();
-    let input = read_input(day);
+    let (day, part) = read_day();
+    let input = read_input(day, part);
 
-    println!("Running for day #{day}");
+    println!("Running for day #{day} part #{part}");
 
     let instant = Instant::now();
-    let output = match day {
-        1 => day_1(input),
-        2 => day_2(input),
-        3 => day_3(input),
-        4 => day_4(input),
-        5 => day_5(input),
-        _ => panic!("Solution not implemented for day #{day}")
+    let output = match (day, part) {
+        (1, 1)  => day_1_part_1(input),
+        (1, 2)  => day_1_part_2(input),
+        (2, 1) => day_2_part_1(input),
+        (2, 2) => day_2_part_2(input),
+        (3, 1) => day_3_part_1(input),
+        (3, 2) => day_3_part_2(input),
+        (4, 1) => day_4_part_1(input),
+        (4, 2) => day_4_part_2(input),
+        _ => panic!("Solution not implemented for day #{day} part #{part}")
     };
 
     println!("Answer: {output}");
     println!("Time: {} ms", instant.elapsed().as_millis())
 }
 
-fn day_1(input: String) -> i32 {
+fn day_1_part_1(input: String) -> i32 {
     let (mut l, mut r): (BinaryHeap<i32>, BinaryHeap<i32>) = input
         .split("\n")
         .filter_map(|line| {
@@ -42,7 +45,29 @@ fn day_1(input: String) -> i32 {
         .sum()
 }
 
-fn day_2(input: String) -> i32 {
+fn day_1_part_2(input: String) -> i32 {
+    let (l, r) = input
+        .split("\n")
+        .filter_map(|line| {
+            line
+                .split_whitespace()
+                .filter_map(|str| str.parse::<i32>().ok())
+                .next_tuple()
+        })
+        .fold((HashMap::new(), HashMap::new()), |mut set: (HashMap<i32, i32>, HashMap<i32, i32>), n: (i32, i32)| {
+            *set.0.entry(n.0).or_insert(0) += n.0;
+            *set.1.entry(n.1).or_insert(0) += 1;
+
+            set
+        });
+
+    l
+        .iter()
+        .fold(0, |acc, (k, n)| acc + n * r.get(k).unwrap_or(&0))
+}
+
+
+fn day_2_part_1(input: String) -> i32 {
     input
         .split("\n")
         .filter(|line| {
@@ -63,7 +88,48 @@ fn day_2(input: String) -> i32 {
         .count() as i32
 }
 
-fn day_3(input: String) -> i32 {
+fn day_2_part_2(input: String) -> i32 {
+    let is_safe = |line: &Vec<i32>| -> bool {
+        let order = line[1] > line[0];
+
+        line
+            .iter()
+            .enumerate()
+            .skip(1)
+            .all(|(i, _)| {
+                let diff = (line[i] - line[i - 1]).abs();
+                let current_order = line[i] > line[i - 1];
+
+                diff >= 1 && diff <= 3 && current_order == order
+            })
+    };
+
+    input
+        .split("\n")
+        .filter(|line| {
+            let mut line = line
+                .split_whitespace()
+                .filter_map(|str| str.parse::<i32>().ok())
+                .collect::<Vec<i32>>();
+
+            if is_safe(&line) {
+                return true;
+            }
+
+            (0..line.len())
+                .any(|i| {
+                    let element = line.remove(i);
+                    let is_safe_now = is_safe(&line);
+                    line.insert(i, element);
+
+                    is_safe_now
+                })
+        })
+        .count() as i32
+}
+
+
+fn day_3_part_1(input: String) -> i32 {
     Regex
         ::new(r"mul\([0-9]+,[0-9]+\)")
         .unwrap()
@@ -80,26 +146,34 @@ fn day_3(input: String) -> i32 {
         })
 }
 
-fn day_4(_: String) -> i32 {
-    0
-}
-
-fn day_5(_: String) -> i32 {
+fn day_3_part_2(_: String) -> i32 {
     0
 }
 
 
-fn read_day() -> i32 {
+fn day_4_part_1(_: String) -> i32 {
+    0
+}
+
+fn day_4_part_2(_: String) -> i32 {
+    0
+}
+
+
+fn read_day() -> (i32, i32) {
     std::env::args()
         .skip(1)
         .take(1)
-        .collect::<String>().parse::<i32>()
-        .expect("Please input day number")
+        .collect::<String>()
+        .split("-")
+        .filter_map(|str| str.parse::<i32>().ok())
+        .next_tuple()
+        .expect("Input arguments should be DAY-PART, for example: 1-2")
 }
 
-fn read_input(day: i32) -> String {
+fn read_input(day: i32, part: i32) -> String {
     let mut input: String = String::new();
-    let input_path = format!("inputs/{}.txt", day);
+    let input_path = format!("inputs/{day}-{part}.txt");
 
     std::fs::File::open(input_path.clone())
         .expect(&format!("Could't open {} file", input_path))
