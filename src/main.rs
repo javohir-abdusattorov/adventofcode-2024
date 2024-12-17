e74212d6644a00b0866a37043215c751efd88068
javohir-abdusattorov <javohirabdusattorov2005@gmail.com>
2024-12-17 15:53:01 +0500

    day #9

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![allow(warnings)]
+
 use std::cmp::Ordering;
 use std::collections::{BinaryHeap, HashMap};
 use std::io::Read;
@@ -13,7 +15,7 @@ fn main() {
     println!("Running for day #{day} part #{part}");
 
     let instant = Instant::now();
-    let output: i32 = match (day, part) {
+    let output = match (day, part) {
         (1, 1) => day_1_part_1(input),
         (1, 2) => day_1_part_2(input),
 
@@ -38,8 +40,8 @@ fn main() {
         (8, 1) => day_8_part_1(input),
         (8, 2) => day_8_part_2(input),
 
-        (9, 1) => day_9_part_1(input),
-        (9, 2) => day_9_part_2(input),
+        (9, 1) => day_9_part_1(input) as i32,
+        (9, 2) => day_9_part_2(input) as i32,
         _ => panic!("Solution not implemented for day #{day} part #{part}")
     };
 
@@ -834,12 +836,71 @@ fn day_8_part_2(input: String) -> i32 {
 }
 
 
-fn day_9_part_1(_: String) -> i32 {
-    0
+fn day_9_part_1(input: String) -> i128 {
+    let mut r = 0;
+    let mut v = input
+        .split("")
+        .filter_map(|str| str.parse::<usize>().ok())
+        .enumerate()
+        .fold((vec![], 0), |(mut acc, p), (index, n)| {
+            if index % 2 != 0 {
+                acc.append(&mut vec![-1; n]);
+                (acc, p)
+            } else {
+                acc.append(&mut vec![p; n]);
+                r = acc.len() - 1;
+                (acc, p + 1)
+            }
+        })
+        .0;
+
+    for i in 0..v.len() {
+        if v[i] != -1 { continue; }
+        if r <= i { break; }
+
+        v.swap(i, r);
+        while v[r] == -1 { r -= 1 }
+    }
+
+    v.into_iter()
+        .enumerate()
+        .filter(|(_, n)| *n != -1)
+        .fold(0, |acc: i128, (i, n)| acc + (n as i128 * i as i128))
 }
 
-fn day_9_part_2(_: String) -> i32 {
-    0
+fn day_9_part_2(input: String) -> i128 {
+    let (mut mem, mut file_slots, mut open_slots, _) = input
+        .split("")
+        .filter_map(|str| str.parse::<usize>().ok())
+        .enumerate()
+        .fold((vec![], vec![], vec![], 0), |(mut acc, mut file_slots, mut open_slots, p), (index, n)| {
+            if index % 2 != 0 {
+                open_slots.push((acc.len(), n));
+                acc.append(&mut vec![-1; n]);
+                (acc, file_slots, open_slots, p)
+            }
+            else {
+                file_slots.push((acc.len(), n));
+                acc.append(&mut vec![p; n]);
+                (acc, file_slots, open_slots, p + 1)
+            }
+        });
+
+    for (i, size) in file_slots.into_iter().skip(1).rev() {
+        if let Some((pos, open_slot)) = open_slots.iter().filter(|slot| slot.0 < i).find_position(|slot| slot.1 >= size) {
+            (0..size).for_each(|k| mem.swap(k + open_slot.0, k + i));
+
+            open_slots[pos] = (open_slots[pos].0 + size, open_slots[pos].1 - size);
+            if open_slots[pos].1 <= 0 {
+                open_slots.remove(pos);
+            }
+        }
+    }
+
+    mem.into_iter()
+        .enumerate()
+        .filter(|(_, n)| *n != -1)
+        .fold(0, |acc: i128, (i, n)| acc + (n as i128 * i as i128))
 }
 
 
